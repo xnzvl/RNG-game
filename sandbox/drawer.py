50938768383550456a9441153112c2bb979d68e7
xnzvl <jakubnezval.jn@gmail.com>
2023-05-27 22:49:02 +0200

    new map

--- sandbox/drawer.py
+++ sandbox/drawer.py
@@ -4,90 +4,91 @@ import tkinter as tk
 import map
 
 
-LIGHT_BLUE = "#4c8ffc"
+UNIT = 100
+CANVAS_CENTER = (5, 5)
 
 
-class AreaWindow:
+class Display_map:
     def __init__(
             self,
-            area: map.Area,
-            location: map.Location_holder
+            map: map.Map
     ) -> None:
-        self.area = area
+        self.map = map
 
         self.root = tk.Tk()
-        self.canvas = tk.Canvas(width=800, height=800)
+        self.canvas = tk.Canvas(width=UNIT * 10, height=UNIT * 10)
         self.canvas.bind("<Button-1>", lambda _: self.refresh())
 
-        self.anchor = 400, 400
         self.root.resizable(False, False)
         self.root.title("MAP-area:testing")
 
-        if location.node is not None:
-            self.refresh()
-
         self.canvas.pack()
+
+        self.refresh()
         self.root.mainloop()
 
-    def refresh(self) -> None:
+    def refresh(
+            self
+    ) -> None:
         self.canvas.delete("all")
 
         self.draw_edges()
         self.draw_nodes()
 
-    def draw_edges(self) -> None:
-        for src in self.area.node_dict.values():
-            for dst in src.get_adjacent():
-                draw_edge(self.canvas, src, dst, self.anchor)
+    def draw_nodes(
+            self
+    ) -> None:
+        for area in self.map.get_areas():
+            for node in area.get_nodes():
+                draw_node(self.canvas, node, area.anchor, self.map)
 
-    def draw_nodes(self) -> None:
-        for node in self.area.node_dict.values():
-            draw_node(self.canvas, node, self.anchor)
+    def draw_edges(
+            self
+    ) -> None:
+        for area in self.map.get_areas():
+            for src in area.get_nodes():
+                for dst in src.get_adjacent():
+                    draw_edge(self.canvas, src, dst, area.anchor)
 
 
 def draw_edge(
         canvas: tk.Canvas,
         node_a: map.Node,
         node_b: map.Node,
-        anchor: Pair[int, int]
+        area_anchor: Pair[int, int]
 ) -> None:
-    x_a, y_a = node_a.position
-    x_b, y_b = node_b.position
+    canvas_x, canvas_y = CANVAS_CENTER
+    area_x, area_y = area_anchor
+    a_x, a_y = node_a.position
+    b_x, b_y = node_b.position
 
-    x_anchor, y_anchor = anchor
-
-    canvas.create_line(x_a + x_anchor, -y_a + y_anchor, x_b + x_anchor, -y_b + y_anchor)
+    canvas.create_line(
+        (a_x + area_x + canvas_x) * UNIT, (-a_y + area_y + canvas_y) * UNIT,
+        (b_x + area_x + canvas_x) * UNIT, (-b_y + area_y + canvas_y) * UNIT,
+        fill="gray"
+    )
 
 
 def draw_node(
         canvas: tk.Canvas,
         node: map.Node,
-        anchor: Pair[int, int]
+        area_anchor: Pair[int, int],
+        map: map.Map
 ) -> None:
-    anchor_x, anchor_y = anchor
+    canvas_x, canvas_y = CANVAS_CENTER
+    area_x, area_y = area_anchor
     node_x, node_y = node.position
-    x = anchor_x + node_x
-    y = anchor_y - node_y
 
-    colour = "red" if node == node.location_holder.node else (
-        "blue" if node == node.location_holder.target else "black"
+    x = (canvas_x + area_x + node_x) * UNIT
+    y = (canvas_y + area_y - node_y) * UNIT
+
+    colour = "red" if node == map.current_node else (
+        "blue" if node == map.current_target else "black"
     )
 
     canvas.create_text(x + 5, y - 5, anchor="sw", text=node.id)
     canvas.tag_bind(
         canvas.create_rectangle(x - 5, y - 5, x + 5, y + 5, fill=colour),
         "<Button-1>",
-        lambda _: set_target(node)
+        lambda _: map.set_target(node)
     )
-
-
-def set_target(
-        target: map.Node
-) -> None:
-    lh = target.location_holder
-
-    if target == lh.target:
-        lh.node = target
-        lh.target = None
-
-    lh.target = target
--- sandbox/map.py
+++ sandbox/map.py
@@ -1,143 +1,217 @@
-from typing import Dict, List, Optional, Set
-from typing import Tuple as Pair
+from typing import List, Optional, Set, Tuple
+
+import abc
 
 import drawer
 
 
-class Location_holder:
-    def __init__(self) -> None:
-        self.node: Optional[Node] = None
-        self.target: Optional[Node] = None
+class Map:
+    def __init__(
+            self
+    ) -> None:
+        self.current_area:   Optional['Area'] = None
+        self.current_node:   Optional['Node'] = None
+        self.current_target: Optional['Node'] = None
+
+        self.areas: Set['Area'] = set()
+
+    def add_area(
+            self,
+            area: 'Area'
+    ) -> None:
+        self.areas.add(area)
+
+    def get_areas(
+            self,
+    ) -> Set['Area']:
+        return self.areas
+
+    def set_area(
+            self,
+            area: 'Area'
+    ) -> None:
+        self.current_area = area
+
+    def set_current_node(
+            self,
+            node: 'Node'
+    ) -> None:
+        self.current_node = node
+
+    def set_target(
+            self,
+            node: 'Node'
+    ) -> None:
+        if self.current_target == node:
+            self.set_current_node(node)
+        else:
+            self.current_target = node
 
 
 class Area:
     def __init__(
             self,
-            area_id: str,
-            area_anchor: Pair[int, int]
+            anchor: Tuple[int, int],
+            id: str
     ) -> None:
-        self.node_dict: Dict[str, Node] = {}
-        self.id = area_id
-        self.anchor = area_anchor
+        self.anchor = anchor
+        self.id = id
+        self.nodes: Set[Node] = set()
 
     def add_node(
             self,
             node: 'Node'
     ) -> 'Node':
-        self.node_dict[node.id] = node
+        self.nodes.add(node)
         return node
 
+    def get_nodes(
+            self
+    ) -> Set['Node']:
+        return self.nodes
 
-class Node:
+
+class Node(abc.ABC):
     def __init__(
             self,
-            node_id: str,
-            node_position: Pair[int, int],
-            location_holder: Location_holder
+            id: str,
+            position: Tuple[int, int]
     ) -> None:
-        self.id = node_id
-        self.position = node_position
-        self.location_holder = location_holder
+        self.id = id
+        self.position = position
 
-    def get_adjacent(self) -> Set['Node']:
-        raise ValueError("should be overriden")
+    @abc.abstractmethod
+    def get_adjacent(
+            self
+    ) -> Set['Node']:
+        ...
 
 
 class Cross_node(Node):
     def __init__(
             self,
-            node_id: str,
-            node_position: Pair[int, int],
-            location_holder: Location_holder
+            id: str,
+            position: Tuple[int, int]
     ) -> None:
-        super().__init__(node_id, node_position, location_holder)
+        super().__init__(id, position)
 
         self.adjacent: Set[Node] = set()
 
-    def get_adjacent(self) -> Set[Node]:
+    def get_adjacent(
+            self
+    ) -> Set[Node]:
         return self.adjacent
+
+    def add_adjacent(
+            self,
+            node: Node
+    ) -> None:
+        self.adjacent.add(node)
 
 
 class Jump_node(Node):
     def __init__(
             self,
             node_id: str,
-            node_position: Pair[int, int],
-            location_holder: Location_holder,
+            node_position: Tuple[int, int],
             jump_target: Node
     ) -> None:
-        super().__init__(node_id, node_position, location_holder)
+        super().__init__(node_id, node_position)
 
         self.jump_target = jump_target
 
-    def get_adjacent(self) -> Set[Node]:
+    def get_adjacent(
+            self
+    ) -> Set[Node]:
         tmp = set()
         tmp.add(self.jump_target)
         return tmp
 
 
 def create_edge(
-        node_a: Node,
-        node_b: Node
+        node_a: Cross_node,
+        node_b: Cross_node
 ) -> None:
-    node_a.get_adjacent().add(node_b)
-    node_b.get_adjacent().add(node_a)
+    node_a.add_adjacent(node_b)
+    node_b.add_adjacent(node_a)
 
 
 def link_node_with(
-        src: Node,
-        targets: List[Node]
+        src: Cross_node,
+        targets: List[Cross_node]
 ) -> None:
     for target in targets:
         create_edge(src, target)
 
 
-def prepare_area(
-        area: Area,
-        location_holder: Location_holder
-) -> None:
-    unit = 175
+def init_map() -> Map:
+    the_map = Map()
 
-    a = area.add_node(Cross_node("A", (0, 0), location_holder))
-    b = area.add_node(Cross_node("B", (-unit, 0), location_holder))
-    c = area.add_node(Cross_node("C", (0, -2 * unit), location_holder))
-    d = area.add_node(Cross_node("D", (-unit, -unit), location_holder))
-    e = area.add_node(Cross_node("E", (unit, 0), location_holder))
-    f = area.add_node(Cross_node("F", (-unit, unit), location_holder))
-    g = area.add_node(Cross_node("G", (-2 * unit, -unit), location_holder))
-    h = area.add_node(Cross_node("H", (-2 * unit, -2 * unit), location_holder))
-    i = area.add_node(Cross_node("I", (unit, 2 * unit), location_holder))
-    j = area.add_node(Cross_node("J", (-2 * unit, 2 * unit), location_holder))
-    k = area.add_node(Cross_node("K", (2 * unit, -2 * unit), location_holder))
-    l = area.add_node(Cross_node("L", (2 * unit, 2 * unit), location_holder))
-    m = area.add_node(Cross_node("M", (2 * unit, unit), location_holder))
-    n = area.add_node(Cross_node("N", (2 * unit, -unit), location_holder))
-    o = area.add_node(Cross_node("O", (0, unit), location_holder))
-    p = area.add_node(Cross_node("P", (-2 * unit, 0), location_holder))
-    q = area.add_node(Cross_node("Q", (-2 * unit, unit), location_holder))
+    area_A = Area((0, 0), "AREA-A")
 
-    location_holder.node = a
+    a = area_A.add_node(Cross_node("A", (-4, 4)))
+    b = area_A.add_node(Cross_node("B", (-3, 4)))
+    c = area_A.add_node(Cross_node("C", (0, 4)))
+    d = area_A.add_node(Cross_node("D", (3, 4)))
+    e = area_A.add_node(Cross_node("E", (4, 4)))
+    f = area_A.add_node(Cross_node("F", (-2, 3)))
+    g = area_A.add_node(Cross_node("G", (1, 3)))
+    h = area_A.add_node(Cross_node("H", (-4, 2)))
+    i = area_A.add_node(Cross_node("I", (0, 2)))
+    j = area_A.add_node(Cross_node("J", (4, 2)))
+    k = area_A.add_node(Cross_node("K", (-2, 1)))
+    l = area_A.add_node(Cross_node("L", (2, 1)))
+    m = area_A.add_node(Cross_node("M", (-3, 0)))
+    n = area_A.add_node(Cross_node("N", (0, 0)))
+    o = area_A.add_node(Cross_node("O", (3, 0)))
+    p = area_A.add_node(Cross_node("P", (-1, -1)))
+    q = area_A.add_node(Cross_node("Q", (2, -1)))
+    r = area_A.add_node(Cross_node("R", (-4, -2)))
+    s = area_A.add_node(Cross_node("S", (0, -2)))
+    t = area_A.add_node(Cross_node("T", (4, -2)))
+    u = area_A.add_node(Cross_node("U", (-2, -3)))
+    v = area_A.add_node(Cross_node("V", (1, -3)))
+    w = area_A.add_node(Cross_node("W", (-4, -4)))
+    x = area_A.add_node(Cross_node("X", (0, -4)))
+    y = area_A.add_node(Cross_node("Y", (3, -4)))
+    z = area_A.add_node(Cross_node("Z", (4, -4)))
 
-    link_node_with(a, [f, b, c, e, k])
-    link_node_with(b, [f, d])
-    link_node_with(d, [b, c])
-    link_node_with(h, [g, c])
-    link_node_with(j, [f, i])
-    link_node_with(m, [i, l])
-    link_node_with(i, [e, m])
-    link_node_with(k, [c])
-    link_node_with(n, [e, k, m])
-    link_node_with(p, [g, d, b])
-    link_node_with(q, [p, f, j])
+    the_map.set_current_node(n)
+    the_map.add_area(area_A)
+
+    link_node_with(a, [b, f, h])
+    link_node_with(b, [a, f, h])
+    link_node_with(c, [f, g, i, k])
+    link_node_with(d, [e, f, g, j])
+    link_node_with(e, [d, g, j, l])
+    link_node_with(f, [a, b, c, d, h, i])
+    link_node_with(g, [c, d, e, i, l])
+    link_node_with(h, [a, b, f, m])
+    link_node_with(i, [c, f, g, k, l])
+    link_node_with(j, [d, e, l, o, q])
+    link_node_with(k, [c, i, m, n, p])
+    link_node_with(l, [d, e, g, i, j])
+    link_node_with(m, [a, h, r, u, w])
+    link_node_with(n, [k, p, s])
+    link_node_with(o, [j, l, q, t])
+    link_node_with(p, [m, n, s, x])
+    link_node_with(q, [j, o, s, t, v])
+    link_node_with(r, [m, u, w])
+    link_node_with(s, [n, p, q, v, x])
+    link_node_with(t, [o, q, v, y, z])
+    link_node_with(u, [k, r, w, x])
+    link_node_with(v, [q, s, t, x, y, z])
+    link_node_with(w, [r, u])
+    link_node_with(x, [p, s, u, v])
+    link_node_with(y, [t, v, z])
+    link_node_with(z, [q, t, v, y])
+
+    return the_map
 
 
 def main() -> None:
-    area = Area("A", (0, 0))
-    location_holder = Location_holder()
-
-    prepare_area(area, location_holder)
-
-    drawer.AreaWindow(area, location_holder)
+    the_map = init_map()
+    drawer.Display_map(the_map)
 
 
 if __name__ == "__main__":
